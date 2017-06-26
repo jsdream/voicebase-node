c1b8fc96c515ea90d5147a382d2f62decbe07723
Vlad Mashkin <jackonerichardson@gmail.com>
2017-06-26 14:45:08 +0300

    Switch from node-rest-client to request-promise

--- test/main.js
+++ test/main.js
@@ -3,5 +3,5 @@ const voicebase = require('../index');
 global.expect = require('chai').expect;
 
 global.api = new voicebase({
-    bearerToken: 'BEARER TOKEN HERE TO RUN TESTS'
+    bearerToken: process.env.VOICEBASE_TOKEN
 });
--- test/resources/media.js
+++ test/resources/media.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 describe('Media Resource', () => {
     it('Should retrieve from the media collection', () => {
         return api.media.get().then((data) => {
@@ -5,9 +7,17 @@ describe('Media Resource', () => {
         });
     });
 
-    it('Should upload new media to the service as an attachment or from a url', () => {
-        return api.media.upload().then((data) => {
-            console.log('data', data);
+    it('Should upload new media to the service as an attachment', () => {
+        return api.media.upload(fs.createReadStream(__dirname + '/../fixtures/test-recording.mp3'), {}, {}, {}).then((data) => {
+            expect(data).to.have.property('mediaId');
+        });
+    });
+
+    it('Should upload new media to the service from a url', () => {
+        const mediaUrl = 'https://s3.amazonaws.com/voicebase-developer-test-content-dev/mpthreetest.mp3';
+
+        return api.media.upload(mediaUrl, {}, {}, {}).then((data) => {
+            expect(data).to.have.property('mediaId');
         });
     });
 });
